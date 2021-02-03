c1e18a9e40f4ccc4df4f873b10a46c2759ec7890
1004gite <1004gite@naver.com>
2021-02-04 04:13:39 +0900

    기본세팅_언어/날짜 변경

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 ## 순서
-* 다운받으면 pip install -r requirements.txt 실행 (virtualenv 다운되어있다는 전제)
-* source bin/activate 실행 (실행 제거 = deactivate)
-* 터미널에서 src 폴더에서 python manage.py runserver 사용시 8000포트에 연결
+
+- 다운받으면 pip install -r requirements.txt 실행 (virtualenv 다운되어있다는 전제)
+- source bin/activate 실행 (실행 제거 = deactivate)
+- python3 manage.py migrate
+- 터미널에서 src 폴더에서 python(python3) manage.py runserver 사용시 8000포트에 연결
 
 (더미파일 localhost:8000/warzone/ 에 있음 그냥 localhost:8000은 에러뜨는게 정상)
 홍일이거 더미 페이지 확인 http://localhost:8000/tft/
--- src/gg_web_crawler/settings.py
+++ src/gg_web_crawler/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'qll37ny7yyz9cs-u@8&0jl$10-it$l9wf6qz2pfv(91^9dz%4c'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,7 +67,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gg_web_crawler.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -80,39 +77,40 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
